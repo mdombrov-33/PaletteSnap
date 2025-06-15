2fc9fb62317d32465f6cf677fa4be80f550eed4f
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-15 17:33:32 +0300

    feat: update github link

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,6 +1,21 @@
+import { FaGithub } from 'react-icons/fa'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+
 function Footer() {
   return (
-    <footer className="shrink-0 text-center text-sm py-2 text-foreground">View on GitHub</footer>
+    <footer className="shrink-0 text-center text-sm py-2 text-foreground">
+      <Button asChild variant="link" className="text-primary hover:text-foreground">
+        <Link
+          href="https://github.com/mdombrov-33/PaletteSnap"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="inline mr-2" />
+          PaletteSnap GitHub
+        </Link>
+      </Button>
+    </footer>
   )
 }
 
